d6898529ed8d0e7fe36555662aa29157b665a04e
Bruno Xu <gbruno.xu@gmail.com>
2015-07-30 18:37:18 +0800

    An sample showing the vertical tabs in TabHost

--- app/src/main/java/com/example/luisalonso/tabhost/MyActivity.java
+++ app/src/main/java/com/example/luisalonso/tabhost/MyActivity.java
@@ -1,8 +1,17 @@
 package com.example.luisalonso.tabhost;
-import android.support.v4.app.FragmentActivity;
+
+import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TabHost;
+import android.widget.TabWidget;
+import android.widget.TextView;
 
 public class MyActivity extends FragmentActivity{
 
@@ -14,28 +23,52 @@ public class MyActivity extends FragmentActivity{
         setContentView(R.layout.activity_my);
         initComponents();
         setOnTabHost();
+
+        TabWidget tw = tabHost.getTabWidget();
+        tw.setOrientation(LinearLayout.VERTICAL);
         animateView();
     }
 
     public void initComponents(){
-        TabHost tabHost = (TabHost)findViewById(R.id.tabHost); tabHost.setup();
+        tabHost = (TabHost)findViewById(R.id.tabHost);
+        tabHost.setup();
     }
 
     public void setOnTabHost(){
         TabHost.TabSpec spec = tabHost.newTabSpec("Tab1");
         spec.setContent(R.id.tabFragmentWelcome);
-        spec.setIndicator("Welcome",getResources().getDrawable(R.drawable.ic_action_social_person));
+//        spec.setIndicator("Welcome",getResources().getDrawable(R.drawable.ic_action_social_person));
+        spec.setIndicator(createIndicatorView(tabHost,"Welcome",getResources().getDrawable(R.drawable.ic_action_social_person)));
         tabHost.addTab(spec);
 
         spec = tabHost.newTabSpec("Tab2");
         spec.setContent(R.id.tabFragmentMaintenance);
-        spec.setIndicator("Maintenance ...",getResources().getDrawable(R.drawable.ic_action_device_access_storage_1));
+//        spec.setIndicator("Maintenance ...",getResources().getDrawable(R.drawable.ic_action_device_access_storage_1));
+        spec.setIndicator(createIndicatorView(tabHost,"Maintenance ...",getResources().getDrawable(R.drawable.ic_action_device_access_storage_1)));
         tabHost.addTab(spec);
 
         spec = tabHost.newTabSpec("Tab3");
         spec.setContent(R.id.tabFragmentConsole);
-        spec.setIndicator("Console",getResources().getDrawable(R.drawable.ic_action_hardware_computer));
+//        spec.setIndicator("Console", getResources().getDrawable(R.drawable.ic_action_hardware_computer));
+        spec.setIndicator(createIndicatorView(tabHost,"Console", getResources().getDrawable(R.drawable.ic_action_hardware_computer)));
         tabHost.addTab(spec);
+
+    }
+    private View createIndicatorView(TabHost tabHost, CharSequence label, Drawable icon) {
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View tabIndicator = inflater.inflate(R.layout.tab_indicator,
+                tabHost.getTabWidget(), // tab widget is the parent
+                false); // no inflate params
+
+        final TextView tv = (TextView) tabIndicator.findViewById(R.id.title);
+        tv.setText(label);
+
+        final ImageView iconView = (ImageView) tabIndicator.findViewById(R.id.icon);
+        iconView.setImageDrawable(icon);
+
+        return tabIndicator;
     }
 
     public void animateView(){
